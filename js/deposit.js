c2c016d78c948fb9db7c3ceee34eb45fa6100989
rashidsarkar <rashidrock558@gmail.com>
2023-08-15 01:08:23 +0600

    new js update jankar vi

--- js/deposit.js
+++ js/deposit.js
@@ -9,3 +9,28 @@ const withdrawAmout = document.getElementById("withdraw-amount");
 
 let depositAmoutDisplay = 0;
 let myTotal = 1200;
+
+// stap 1 add to the deposit btn
+depositBtn.addEventListener("click", function () {
+  // stap 2
+  const newDepositFildString = depositInputFild.value;
+  const newDepositAmount = parseFloat(newDepositFildString);
+  //steap 3 get depost total
+  const prevesFepositTotalString = depositAmout.innerText;
+  const preveDepositTotal = parseFloat(prevesFepositTotalString);
+  // stap 4 add number
+
+  const currentDepositTotal = preveDepositTotal + newDepositAmount;
+  //set deposit total
+  depositAmout.innerText = currentDepositTotal;
+  //step 5 ballace total
+  const prebeusBalanceTotalString = myAmount.innerText;
+  const preveBalanTotal = parseFloat(prebeusBalanceTotalString);
+  // steap 6 calcutate curent total
+  const currtentBallanceTotal = preveBalanTotal + newDepositAmount;
+  // set balnce total
+  myAmount.innerText = currtentBallanceTotal;
+
+  //steap 7
+  depositInputFild.value = "";
+});
